d780737fbf7473b48bae1c9073240364d4f85926
thecontinium <thecontinium@outlook.com>
2016-01-17 21:34:27 +0530

    .

--- Formula/showloginwindow.rb
+++ Formula/showloginwindow.rb
@@ -1,11 +1,13 @@
 class Showloginwindow< Formula
   homepage 'https://github.com/thecontinium/homebrew-showloginwindow/'
   url 'https://github.com/thecontinium/homebrew-showloginwindow.git'
-  version '0.2.0'
+  version '0.3.0'
 
   skip_clean 'bin'
 
   def install
+    ohai "No executables will be installed"
+    #bin.install 'donothing'
   end
 
   #plist_options :startup => true
