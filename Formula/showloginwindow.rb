47d00029a2d335cbb10d0742c35a8b5984681832
thecontinium <thecontinium@outlook.com>
2016-01-17 20:57:51 +0530

    Put plist in formula

--- Formula/showloginwindow.rb
+++ Formula/showloginwindow.rb
@@ -9,6 +9,32 @@ class Showloginwindow< Formula
     prefix.install Dir["config/*.plist"]
   end
 
-  plist_options :startup => true
+  #plist_options :startup => true
 
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+	    <key>AbandonProcessGroup</key>
+	    <true/>
+      <key>Disabled</key>
+      <false/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+		    <string>/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession</string>
+		    <string>-suspend</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>/tmp/#{plist_name}.err</string>
+      <key>StandardOutPath</key>
+      <string>/tmp/#{plist_name}.out</string>
+    </dict>
+    </plist>
+    EOS
+  end
 end
