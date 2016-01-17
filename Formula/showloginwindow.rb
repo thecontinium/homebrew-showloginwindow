cff799f98e92adc7a41d3c46a58f859a9b87268e
thecontinium <thecontinium@outlook.com>
2016-01-17 21:06:04 +0530

    install nothing and bump

--- Formula/showloginwindow.rb
+++ Formula/showloginwindow.rb
@@ -1,12 +1,11 @@
 class Showloginwindow< Formula
   homepage 'https://github.com/thecontinium/homebrew-showloginwindow/'
   url 'https://github.com/thecontinium/homebrew-showloginwindow.git'
-  version '0.0.2'
+  version '0.0.3'
 
   skip_clean 'bin'
 
   def install
-    prefix.install Dir["config/*.plist"]
   end
 
   #plist_options :startup => true
