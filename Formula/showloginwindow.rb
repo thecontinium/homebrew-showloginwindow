06a2ee9838ab8e4fa4d79980459cbe50cd75d8c9
thecontinium <thecontinium@outlook.com>
2016-01-18 10:26:27 +0530

    .

--- Formula/showloginwindow.rb
+++ Formula/showloginwindow.rb
@@ -1,7 +1,7 @@
 class Showloginwindow< Formula
   homepage 'https://github.com/thecontinium/homebrew-showloginwindow/'
   url 'https://github.com/thecontinium/homebrew-showloginwindow.git'
-  version '0.8.0'
+  version '0.10.0'
 
   skip_clean 'bin'
 
