03c8d9355871351998a682e42fd52071fff01442
thecontinium <thecontinium@outlook.com>
2016-01-18 10:35:26 +0530

    .

--- Formula/showloginwindow.rb
+++ Formula/showloginwindow.rb
@@ -1,43 +1,13 @@
 class Showloginwindow< Formula
   homepage 'https://github.com/thecontinium/homebrew-showloginwindow/'
   url 'https://github.com/thecontinium/homebrew-showloginwindow.git'
-  version '0.11.0'
+  version '0.12.0'
 
   skip_clean 'bin'
 
   def install
     ohai "No executables will be installed"
-    system "echo hello"
-    #bin.install 'donothing'
     prefix.install Dir["config/*.plist"]
   end
 
-  #plist_options :startup => true
-
-  #def plist; <<-EOS.undent
-  #  <?xml version="1.0" encoding="UTF-8"?>
-  #  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-  #  <plist version="1.0">
-  #  <dict>
-	#    <key>AbandonProcessGroup</key>
-	#    <true/>
-  #    <key>Disabled</key>
-  #    <false/>
-  #    <key>Label</key>
-  #    <string>#{plist_name}</string>
-  #    <key>ProgramArguments</key>
-  #    <array>
-	#	    <string>/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession</string>
-	#	    <string>-suspend</string>
-  #    </array>
-  #    <key>RunAtLoad</key>
-  #    <true/>
-  #    <key>StandardErrorPath</key>
-  #    <string>/tmp/#{plist_name}.err</string>
-  #    <key>StandardOutPath</key>
-  #    <string>/tmp/#{plist_name}.out</string>
-  #  </dict>
-  #  </plist>
-  #  EOS
-  #end
 end
